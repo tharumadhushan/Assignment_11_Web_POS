d037df6bb6b3278f37f08d106bba69005600eb74
tharumadhushan <tharushamadhushan555@gmail.com>
2023-10-21 10:55:15 +0530

    Initial commit

---
+++ cyber_store/controller/OrderController.js
@@ -0,0 +1,175 @@
+import {OrderModel} from "../model/OrderModel.js";
+import {order_db} from "../db/db.js";
+
+var row_index = null;
+
+const clear = () => {
+    $("#order-id").val("");
+    $("#order-name").val("");
+    $("#cus-name").val("");
+    $("#unit-price").val("");
+    $("#qty").val("");
+    $("#total").val("");
+}
+
+const loadOrderData = () => {
+    $('#order-tbl-body').empty(); // make tbody empty
+    order_db.map((item, index) => {
+        let record = `<tr><td class="order_id">${item.order_id}</td><td class="order_name">${item.order_name}</td><td class="cus_name">${item.cus_name}</td><td class="unit_price">${item.unit_price}</td><td class="qty">${item.qty}</td><td class="total">${item.total}</td></tr>`;
+        $("#order-tbl-body").append(record);
+    });
+};
+
+// submit
+$("#order-btns>button[type='button']").eq(0).on("click", () => {
+    let order_id = $("#order-id").val();
+    let order_name = $("#order-name").val();
+    let cus_name = $("#cus-name").val();
+    let unit_price = $("#unit-price").val();
+    let qty = $("#qty").val();
+    let total = $("#total").val();
+
+    if (order_id){
+        if (order_name){
+            if (cus_name){
+                if (unit_price){
+                    if (qty){
+                        if (total){
+
+                            let student_obj = new OrderModel(order_id,order_name,cus_name,unit_price,qty,total);
+
+                            // save in the db
+                            order_db.push(student_obj);
+
+                            // clear();
+                            $("#order-btns>button[type='reset']").click();
+
+                            // load student data
+                            loadOrderData();
+                        }else {
+                            Swal.fire({
+                                icon: 'error',
+                                title: 'Invalid Input',
+                                text: 'Please enter total!'
+                            })
+                        }
+                    }else {
+                        Swal.fire({
+                            icon: 'error',
+                            title: 'Invalid Input',
+                            text: 'Please enter qty!'
+                        })
+                    }
+                }else {
+                    Swal.fire({
+                        icon: 'error',
+                        title: 'Invalid Input',
+                        text: 'Please enter unit price!'})
+                }
+            }else {
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Invalid Input',
+                    text: 'Please enter cus name!'})
+            }
+        }else {
+            Swal.fire({
+                icon: 'error',
+                title: 'Invalid Input',
+                text: 'Please enter Order name!'
+            })
+        }
+    }else {
+        Swal.fire({
+            icon: 'error',
+            title: 'Invalid Input',
+            text: 'Please enter orderId!'})
+    }
+
+});
+
+// update
+$("#order-btns>button[type='button']").eq(1).on("click", () => {
+
+    let order_id = $("#order-id").val();
+    let order_name = $("#order-name").val();
+    let cus_name = $("#cus-name").val();
+    let unit_price = $("#unit-price").val();
+    let qty = $("#qty").val();
+    let total = $("#total").val();
+
+    let student_obj = new OrderModel(order_id,order_name,cus_name,unit_price,qty,total);
+
+    // find item index
+    let index = order_db.findIndex(item => item.order_id === order_id);
+
+    // update item in the db
+    order_db[index] = student_obj;
+
+    // clear();
+    $("#order-btns>button[type='reset']").click();
+
+    // load student data
+    loadOrderData();
+})
+
+// delete
+$("#order-btns>button[type='button']").eq(2).on("click", () => {
+
+    //delete validation
+    Swal.fire({
+        title: 'Are you sure?',
+        text: "You won't be able to revert this!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes, delete it!'
+       }).then((result) => {
+        if (result.isConfirmed) {
+
+            let order_id = $("#order-id").val();
+
+            // find item index
+            let index = order_db.findIndex(item => item.order_id === order_id);
+
+            // remove the item from the db
+            order_db.splice(index, 1);
+
+            $("#order-btns>button[type='reset']").click();
+
+            // load student data
+            loadOrderData();
+
+            Swal.fire(
+                'Deleted!',
+                'Your file has been deleted.',
+                'success'
+            )
+        }
+    })
+
+})
+
+$("#order-tbl-body").on("click", "tr", function() {
+    row_index = $(this).index();
+
+    console.log(row_index);
+
+    let order_id = $(this).find(".order_id").text();
+    let order_name = $(this).find(".order_name").text();
+    let cus_name = $(this).find(".cus_name").text();
+    let unit_price = $(this).find(".unit_price").text();
+    let qty = $(this).find(".qty").text();
+    let total = $(this).find(".total").text();
+
+    $("#order-id").val(order_id);
+    $("#order-name").val(order_name);
+    $("#cus-name").val(cus_name);
+    $("#unit-price").val(unit_price);
+    $("#qty").val(qty);
+    $("#total").val(total);
+
+
+
+});
